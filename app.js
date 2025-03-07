a94146dcf294fc2a126d17f17ff575aa67810b68
tonmoy <tonmoy3673@gmail.com>
2025-03-07 18:53:09 +0600

    :zap: disable future date

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log('connected');
+// =============== disable future date ==========//
+let inputDate = document.getElementById('date');
+inputDate.max = new Date().toISOString().split('T')[0];
