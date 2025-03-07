c3ebf8595decc0aa64020940779413d15125c26c
tonmoy <tonmoy3673@gmail.com>
2025-03-08 00:25:10 +0600

    :bulb: make calculate button disabled based on consition

--- app.js
+++ app.js
@@ -2,3 +2,34 @@ console.log('connected');
 // =============== disable future date ==========//
 let inputDate = document.getElementById('date');
 inputDate.max = new Date().toISOString().split('T')[0];
+
+// ========= get button id ===========//
+const button =  document.getElementById('cal-btn');
+button.disabled = !inputDate.value;
+
+
+// ========== check input value -======//
+
+inputDate.addEventListener('input',function () {
+    button.disabled = !inputDate.value;
+})
+
+// ======== Calculate Birthdate ==========//
+
+const calculateBirthdate =()=>{
+
+    // ========== Get Birthday Date ==========//
+const birthDate = new Date(inputDate.value);
+console.log(birthDate);
+let d1 = birthDate.getDate();
+let m1 = birthDate.getMonth()+1;
+let y1 = birthDate.getFullYear();
+console.log(d1,m1,y1);
+
+// ============ Get Current Date -========//
+const currentDate = new Date();
+    
+
+
+
+}
